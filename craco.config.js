0029a4c2ef9c3244c46877945e26bc2df0a959f2
Nikunt Kumawat <143009832+Nikuntkumawat@users.noreply.github.com>
2024-03-30 16:54:50 +0530

    Afford Medical Test

---
+++ craco.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    configure: {
+      resolve: {
+        fallback: {
+          "zlib": require.resolve("browserify-zlib"),
+          "querystring": require.resolve("querystring-es3"),
+          "path": require.resolve("path-browserify"),
+          "crypto": require.resolve("crypto-browserify"),
+          "stream": require.resolve("stream-browserify"),
+          "http": require.resolve("stream-http"),
+          "url": require.resolve("url/"),
+          "util": require.resolve("util/")
+        }
+      },
+    },
+  },
+};
